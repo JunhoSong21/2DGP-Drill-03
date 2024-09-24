4066f3fea8ad79c15486823d295562c7bb2f94a3
JunhoSong21 <junhoasd1zxc2@gmail.com>
2024-09-24 10:55:35 +0900

    사각형 이동 함수 구현

--- character_grass.py
+++ character_grass.py
@@ -13,23 +13,30 @@ def draw_boy(x, y):
     boy.draw_now(x, y)
     delay(0.01)
 
-def run_top():
-    print('TOP')
-    for x in range(800, 0, -10):
-        draw_boy(x, 550)
-    pass
+def run_bottom():
+    print('BOTTOM')
+    for x in range(400, 780, 10):
+        draw_boy(x, 90)
 
 def run_right():
     print('RIGHT')
-    pass
+    for y in range(90, 550, 10):
+        draw_boy(780, y)
 
-def run_bottom():
-    print('BOTTOM')
-    pass
+def run_top():
+    print('TOP')
+    for x in range(780, 20, -10):
+        draw_boy(x, 550)
 
 def run_left():
     print('LEFT')
-    pass
+    for y in range(550, 90, -10):
+        draw_boy(20, y)
+
+def run_second_bottom():
+    print('BOTTOM')
+    for x in range(20, 400, 10):
+        draw_boy(x, 90)
 
 def run_rectangle():
     print('RECTANGLE')
@@ -37,6 +44,7 @@ def run_rectangle():
     run_right()
     run_top()
     run_left()
+    run_second_bottom()
     pass
 
 def run_circle():
@@ -55,6 +63,5 @@ def run_circle():
 while True:
     run_rectangle()
     run_circle()
-    break
 
 close_canvas()
