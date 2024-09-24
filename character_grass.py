111087940ccdb13bf6bc57e46b4bef20f048b6d8
JunhoSong21 <junhoasd1zxc2@gmail.com>
2024-09-24 10:02:19 +0900

    화면에 캐릭터 출력, 브레이크 포인트 추가

--- character_grass.py
+++ character_grass.py
@@ -1,14 +1,17 @@
 from pico2d import *
 
+open_canvas()
+
 def run_rectangle():
     print('RECTANGLE')
     pass
 
 def run_circle():
     print('CIRCLE')
+    clear_canvas_now()
+    boy.draw_now(400, 300)
+    delay(1)
     pass
-
-open_canvas()
 
 # fill here
 grass = load_image('grass.png')
@@ -17,5 +20,6 @@ boy = load_image('character.png')
 while True:
     run_rectangle()
     run_circle()
+    break
 
 close_canvas()
